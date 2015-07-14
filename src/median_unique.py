26dcadf74d465aad47bbe79720d65cd949ad4307
ikoroe <ikoroe@gmail.com>
2015-07-14 22:57:29 +0100

    Update median_unique.py

--- src/median_unique.py
+++ src/median_unique.py
@@ -1,4 +1,13 @@
-with open('tweets.txt','r') as f:
-    for line in f.readlines():
-        uniquewords = len(line.split(' '))
-        print words
+# a program that will print the number of unique words per line
+medians = []
+unique_words_per_tweet = []
+with open('tweets.txt', "r") as f:
+        for line in f.readlines():
+            unique_words = set(line.split())
+            unique_words = len(unique_words)
+            unique_words_per_tweet.append(unique_words)
+            new_median = sum(unique_words_per_tweet) + unique_words/len(unique_words_per_tweet) + 1
+            medians.append(new_median)
+
+#write content of medians into your new f2.txt file here
+
